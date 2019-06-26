a3c0462746cba63e101f15bd074f1e94e35c7bb3
Antonio Bertucci <archano@gmail.com>
2019-06-26 15:57:29 +0200

    Add extensions to Android plugin's Groovy DSL for BaseFlavor

--- plugin/core/src/main/groovy/com/glovo/mobile/release/MobileReleaseGroovyPlugin.groovy
+++ plugin/core/src/main/groovy/com/glovo/mobile/release/MobileReleaseGroovyPlugin.groovy
@@ -1,13 +1,24 @@
 package com.glovo.mobile.release
 
-
+import com.glovo.mobile.release.internal.PropertiesFile
+import com.glovo.mobile.release.dsl.BaseFlavorGroovyDSL
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class MobileReleaseGroovyPlugin implements Plugin<Project> {
 
     @Override
-    void apply(Project target) {
+    void apply(Project project) {
         println("Applied ${this.class.canonicalName}")
+
+        project.ext.propertiesFile = { path -> new PropertiesFile(project.file(path)) }
+
+        project.plugins.withId('com.android.application') {
+            BaseFlavorGroovyDSL.extend(project.android.defaultConfig)
+            project.android.productFlavors.all {
+                BaseFlavorGroovyDSL.extend(it)
+            }
+        }
     }
+
 }
