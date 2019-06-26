c67f3c0ded77b4b3683111b1f36d6b02c475ecc0
Antonio Bertucci <archano@gmail.com>
2019-06-26 19:24:15 +0200

    Embed project building facilities into TestProject

--- plugin/functional-tests/src/test/groovy/com/glovo/mobile/release/MobileReleaseGroovyPluginTest.groovy
+++ plugin/functional-tests/src/test/groovy/com/glovo/mobile/release/MobileReleaseGroovyPluginTest.groovy
@@ -1,7 +1,6 @@
 package com.glovo.mobile.release
 
 import com.glovo.test.TestProject
-import org.gradle.testkit.runner.GradleRunner
 import org.junit.Test
 
 import static com.google.common.truth.Truth.assertThat
@@ -12,11 +11,7 @@ class MobileReleaseGroovyPluginTest {
     void appliesPluginCorrectly() {
         def project = TestProject.groovy('simple-test-groovy')
 
-        def result = GradleRunner.create()
-                .withProjectDir(project.projectDir)
-                .withArguments('clean')
-                .forwardOutput()
-                .build()
+        def result = project.build('clean')
 
         assertThat(result.output).contains('Applied com.glovo.mobile.release.MobileReleaseGroovyPlugin')
     }
