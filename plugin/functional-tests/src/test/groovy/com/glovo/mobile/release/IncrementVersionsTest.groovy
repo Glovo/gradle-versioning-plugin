518741ce5e00fded4bf278ebbba8c93dd59094aa
Antonio Bertucci <archano@gmail.com>
2019-06-27 18:26:59 +0200

    Register task to increment persisted semver when file used as source for versionName

---
+++ plugin/functional-tests/src/test/groovy/com/glovo/mobile/release/IncrementVersionsTest.groovy
@@ -0,0 +1,91 @@
+package com.glovo.mobile.release
+
+import com.glovo.test.TestProject
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+import static com.google.common.truth.Truth.assertThat
+
+@RunWith(Parameterized)
+class IncrementVersionsTest {
+
+    @Parameterized.Parameters
+    static def parameters() {
+        return [
+                { name -> createGroovyProject(name) },
+                { name -> createKotlinProject(name) }
+        ]
+    }
+
+    private final Closure<TestProject> createProject
+
+    IncrementVersionsTest(Closure<TestProject> createProject) {
+        this.createProject = createProject
+    }
+
+    @Test
+    void bumpsPatchVersionByDefault() {
+        TestProject project = createProject('increment-default')
+        def versions = project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1'])
+
+        project.build('incrementMainVersionName')
+
+        assertThat(versions.text).contains('versionName=1.1.2')
+    }
+
+    @Test
+    void bumpsMajorVersion() {
+        TestProject project = createProject('increment-major')
+        def versions = project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1'])
+
+        project.build('incrementMainVersionName', '--major')
+
+        assertThat(versions.text).contains('versionName=2.0.0')
+    }
+
+    @Test
+    void bumpsMinorVersion() {
+        TestProject project = createProject('increment-groovy-minor')
+        def versions = project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1'])
+
+        project.build('incrementMainVersionName', '--minor')
+
+        assertThat(versions.text).contains('versionName=1.2.0')
+    }
+
+    @Test
+    void bumpsPatchVersion() {
+        TestProject project = createProject('increment-groovy-patch')
+        def versions = project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1'])
+
+        project.build('incrementMainVersionName', '--patch')
+
+        assertThat(versions.text).contains('versionName=1.1.2')
+    }
+
+    private static TestProject createGroovyProject(String projectName) {
+        def project = TestProject.groovy("$projectName-groovy")
+        project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        persistedVersionName from: file('versions.properties')
+                    }
+                }
+                """
+        return project
+    }
+
+    private static TestProject createKotlinProject(String projectName) {
+        def project = TestProject.kotlin("$projectName-kotlin")
+        project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        persistedVersionName(from=file("versions.properties"))
+                    }
+                }
+                """
+        return project
+    }
+
+}
