58010192ed325c31934192617e38a49282326d30
Antonio Bertucci <archano@gmail.com>
2019-07-02 15:27:33 +0200

    Add missing functional tests about overriding versionName/versionCode

--- plugin/functional-tests/src/test/groovy/com/glovo/mobile/release/PersistedVersionsTest.groovy
+++ plugin/functional-tests/src/test/groovy/com/glovo/mobile/release/PersistedVersionsTest.groovy
@@ -50,7 +50,7 @@ class PersistedVersionsTest {
         }
 
         @Test
-        void usesVersionCodeFromSpecifiedPropertiedFile() {
+        void usesVersionCodeFromSpecifiedPropertiesFile() {
             def project = TestProject.groovy('versions-groovy-3')
             project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
             project.appBuildScript += """\
@@ -68,12 +68,70 @@ class PersistedVersionsTest {
             assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 4321;')
         }
 
+        @Test
+        void allowsToOverrideVersionName() {
+            def project = TestProject.groovy('versions-groovy-2')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        persistedVersionName from: file('versions.properties')
+                        versionName '0.0.0'
+                        versionCode 1
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1;')
+        }
+
+        @Test
+        void allowsToOverrideVersionCode() {
+            def project = TestProject.groovy('versions-groovy-3')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        versionName '0.0.0'
+                        persistedVersionCode from: file('versions.properties')
+                        versionCode 1234
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1234;')
+        }
+
+        @Test
+        void doesNotRequireUsingPropertiesFiles() {
+            def project = TestProject.groovy('versions-groovy-3')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        versionName '0.0.0'
+                        versionCode 1234
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1234;')
+        }
     }
 
     static class UsingKotlinScript {
 
         @Test
-        void usesVersionNameAndVersionCodeFromSpecifiedPropertiedFile() {
+        void usesVersionNameAndVersionCodeFromSpecifiedPropertiesFile() {
             def project = TestProject.kotlin('versions-kotlin-1')
             project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
             project.appBuildScript += """\
@@ -91,7 +149,7 @@ class PersistedVersionsTest {
         }
 
         @Test
-        void usesVersionNameFromSpecifiedPropertiedFile() {
+        void usesVersionNameFromSpecifiedPropertiesFile() {
             def project = TestProject.kotlin('versions-kotlin-2')
             project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
             project.appBuildScript += """\
@@ -110,7 +168,7 @@ class PersistedVersionsTest {
         }
 
         @Test
-        void usesVersionCodeFromSpecifiedPropertiedFile() {
+        void usesVersionCodeFromSpecifiedPropertiesFile() {
             def project = TestProject.kotlin('versions-kotlin-3')
             project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
             project.appBuildScript += """\
@@ -128,6 +186,64 @@ class PersistedVersionsTest {
             assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 4321;')
         }
 
+        @Test
+        void allowsToOverrideVersionName() {
+            def project = TestProject.kotlin('versions-kotlin-2')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        persistedVersionName(from=file("versions.properties"))
+                        versionName = "0.0.0"
+                        versionCode = 1
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1;')
+        }
+
+        @Test
+        void allowsToOverrideVersionCode() {
+            def project = TestProject.kotlin('versions-kotlin-3')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        versionName = "0.0.0"
+                        persistedVersionCode(from=file("versions.properties"))
+                        versionCode = 1234
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1234;')
+        }
+
+        @Test
+        void doesNotRequireUsingPropertiesFiles() {
+            def project = TestProject.groovy('versions-groovy-3')
+            project.newPropertiesFile('app/versions.properties', [versionName: '1.1.1', versionCode: 4321])
+            project.appBuildScript += """\
+                android {
+                    defaultConfig {
+                        versionName = "0.0.0"
+                        versionCode = 1234
+                    }
+                }
+                """
+
+            project.build('assembleDebug')
+
+            assertThat(project.generatedBuildConfig).contains('public static final String VERSION_NAME = "0.0.0";')
+            assertThat(project.generatedBuildConfig).contains('public static final int VERSION_CODE = 1234;')
+        }
     }
 
 }
