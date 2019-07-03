e7793545ff64e37382b1fcb293a57f383c9c0c0a
Antonio Bertucci <archano@gmail.com>
2019-07-03 12:33:44 +0200

    Use ECS main agent to build android demo app

--- demo/android/ci/buildStages.groovy
+++ demo/android/ci/buildStages.groovy
@@ -1,10 +1,12 @@
 def call(pipelineParams) {
 
-    stage('Build') {
-        dir('demo/android') {
-            echo 'Start build'
-            sh './gradlew :app:assemble'
-            echo 'Build finished'
+    ecsAgent(pipelineParams.mainAgent) {
+        stage('Build') {
+            dir('demo/android') {
+                echo 'Start build'
+                sh './gradlew :app:assemble'
+                echo 'Build finished'
+            }
         }
     }
 
