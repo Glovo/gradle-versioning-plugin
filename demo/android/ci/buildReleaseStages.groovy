b881288d54f62c32573380a8ffde7a509d8ed4fb
Antonio Bertucci <archano@gmail.com>
2019-07-18 18:55:18 +0200

    Extract deploy to HockeyApp as separate stage

--- demo/android/ci/buildReleaseStages.groovy
+++ demo/android/ci/buildReleaseStages.groovy
@@ -10,28 +10,27 @@ def call(pipelineParams, stageConfig, stageParams, input) {
         stage('Build') {
             checkout scm
             dir('demo/android') {
-                echo 'Start build'
                 sh './gradlew --no-daemon :app:assemble'
-                echo 'Build finished'
-                echo 'Pushing to HockeyApp'
-                withCredentials([string(credentialsId: 'hockey-app-token', variable: 'HOCKEY_API_TOKEN')]) {
-                    hockeyApp(
-                            applications: [
-                                    [
-                                            apiToken          : "$HOCKEY_API_TOKEN",
-                                            downloadAllowed   : true,
-                                            filePath          : 'app/build/outputs/apk/debug/app-debug.apk',
-                                            mandatory         : true,
-                                            notifyTeam        : true,
-                                            releaseNotesMethod: manual(releaseNotes: readReleaseNotes(), isMarkDown: true),
-                                            uploadMethod      : versionCreation(appId: 'e4bacf5c548547b0bf48a37b671b1bad')
-                                    ]
-                            ],
-                            baseUrl: '',
-                            debugMode: true,
-                            failGracefully: false
-                    )
-                }
+            }
+        }
+        stage('Deploy to HockeyApp') {
+            withCredentials([string(credentialsId: 'hockey-app-token', variable: 'HOCKEY_API_TOKEN')]) {
+                hockeyApp(
+                        applications: [
+                                [
+                                        apiToken          : "$HOCKEY_API_TOKEN",
+                                        downloadAllowed   : true,
+                                        filePath          : 'demo/android/app/build/outputs/apk/debug/app-debug.apk',
+                                        mandatory         : true,
+                                        notifyTeam        : true,
+                                        releaseNotesMethod: manual(releaseNotes: readReleaseNotes(), isMarkDown: true),
+                                        uploadMethod      : versionCreation(appId: 'e4bacf5c548547b0bf48a37b671b1bad')
+                                ]
+                        ],
+                        baseUrl: '',
+                        debugMode: true,
+                        failGracefully: false
+                )
             }
         }
     }
