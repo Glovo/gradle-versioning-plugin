3c26cfc5918e5617e5b6a8b6b6ef88820349278d
Adam Szewera <adam.szewe@gmail.com>
2020-09-14 00:15:25 +0200

    [Tech] Update Kotlin version

--- plugin/functional-tests/src/test/groovy/com/glovo/test/TestProject.groovy
+++ plugin/functional-tests/src/test/groovy/com/glovo/test/TestProject.groovy
@@ -67,7 +67,7 @@ class TestProject {
                 }
                 dependencies {
                     classpath("com.android.tools.build:gradle:4.0.1")
-                    classpath(kotlin("gradle-plugin", version = "1.3.31"))
+                    classpath(kotlin("gradle-plugin", version = "1.3.72"))
                 }
             }
             
