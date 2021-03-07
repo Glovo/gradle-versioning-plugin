2bdb743368422b849dc0e9709728241d138666b3
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-03-07 12:21:14 +0100

    Added more parameters to `IncrementSemanticVersionTask`

--- plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementSemanticVersionTask.kt
+++ plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementSemanticVersionTask.kt
@@ -3,7 +3,9 @@ package com.glovoapp.versioning.tasks
 import com.glovoapp.versioning.PersistedVersion
 import com.glovoapp.versioning.SemanticVersion
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
 
 open class IncrementSemanticVersionTask : DefaultTask() {
@@ -12,26 +14,64 @@ open class IncrementSemanticVersionTask : DefaultTask() {
     lateinit var version: PersistedVersion<SemanticVersion>
 
     @Input
+    @Option(option = "increment", description = "The increment type")
     var increment = SemanticVersion.Increment.PATCH
+
+    @Input
+    var amount = 1
+
+    @Input
+    @Option(option = "preserve-pre-release", description = "Add or replace current pre-release labels")
+    var preservePreRelease = false
+
+    @Input
+    @Option(option = "preserve-build", description = "Add or replace current build labels")
+    var preserveBuild = false
+
+    @Input
+    @Option(option = "pre-release", description = "Adds pre-release labels to the version")
+    var preReleaseLabels = mutableListOf<String>()
+
+    @Input
+    @Option(option = "build", description = "Adds build labels to the version")
+    var buildLabels = mutableListOf<String>()
 
     @Option(option = "major", description = "Increments major version")
     fun major() {
-        increment = SemanticVersion.Increment.MAJOR
+        this.increment = SemanticVersion.Increment.MAJOR
     }
 
     @Option(option = "minor", description = "Increments minor version")
     fun minor() {
-        increment = SemanticVersion.Increment.MINOR
+        this.increment = SemanticVersion.Increment.MINOR
     }
 
     @Option(option = "patch", description = "Increments patch version")
     fun patch() {
-        increment = SemanticVersion.Increment.PATCH
+        this.increment = SemanticVersion.Increment.PATCH
+    }
+
+    @Option(option = "amount", description = "The increment amount")
+    fun amount(amount: String) {
+        this.amount = checkNotNull(amount.toIntOrNull()?.takeIf { it >= 0 }) { "amount `$amount` must be >= 0" }
+    }
+
+    @Input
+    @Option(option = "preserve", description = "Add or replace current pre-release and build labels")
+    fun preserve() {
+        this.preservePreRelease = true
+        this.preserveBuild = true
     }
 
     @TaskAction
-    fun run() {
-        version.value += increment
+    fun run() = with(version) {
+        val prLabels = if (preservePreRelease) value.preReleaseIdentifiers + preReleaseLabels else preReleaseLabels
+        val bLabels = if (preservePreRelease) value.buildIdentifiers + buildLabels else buildLabels
+
+        value = (value + increment * amount).copy(
+            preRelease = prLabels.joinToString(separator = "."),
+            build = bLabels.joinToString(separator = ".")
+        )
     }
 
 }
--- plugin/src/test/kotlin/com/glovoapp/versioning/SemanticVersioningPluginTest.kt
+++ plugin/src/test/kotlin/com/glovoapp/versioning/SemanticVersioningPluginTest.kt
@@ -2,11 +2,11 @@ package com.glovoapp.versioning
 
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class SemanticVersioningPluginTest() {
 
@@ -17,27 +17,71 @@ class SemanticVersioningPluginTest() {
     }
 
     @ParameterizedTest
-    @EnumSource(SemanticVersion.Increment::class)
-    fun incrementSemanticVersion(increment: SemanticVersion.Increment?) = with(gradle) {
-        versionFile("version" to "1.2.3")
-
-        val taskParam = increment?.let { "--${it.name.toLowerCase()}" }
-        val expectedVersion = when (increment) {
-            SemanticVersion.Increment.MAJOR -> "2.0.0"
-            SemanticVersion.Increment.MINOR -> "1.3.0"
-            SemanticVersion.Increment.PATCH, null -> "1.2.4"
-        }
+    @MethodSource("incrementParams")
+    fun incrementSemanticVersion(
+        currentVersion: String,
+        taskParams: Array<String>,
+        expectedVersion: String
+    ) = with(gradle) {
+        versionFile("version" to currentVersion)
 
         val result = runner
-            .withArguments(listOfNotNull("-s", "incrementSemanticVersion", taskParam))
+            .withArguments(listOfNotNull("-s", "incrementSemanticVersion", *taskParams))
             .build()
 
         assertEquals(TaskOutcome.SUCCESS, result.task(":incrementSemanticVersion")?.outcome)
-        assertTrue("version=$expectedVersion" in versionFile.readLines())
+        assertEquals("version=$expectedVersion", versionFile.readLines().drop(1).firstOrNull())
     }
 
-    @Test
-    fun incrementSemanticVersion_default() =
-        incrementSemanticVersion(null)
+    companion object {
+
+        @JvmStatic
+        fun incrementParams() = Stream.of(
+            of("1.2.3", emptyArray<String>(), "1.2.4"),
+            of("1.2.3", arrayOf("--patch"), "1.2.4"),
+            of("1.2.3", arrayOf("--minor"), "1.3.0"),
+            of("1.2.3", arrayOf("--major"), "2.0.0"),
+            of("4.0.0", arrayOf("--amount=3"), "4.0.3"),
+            of("4.0.0", arrayOf("--increment=patch", "--amount=3"), "4.0.3"),
+            of("4.0.0", arrayOf("--increment=minor", "--amount=3"), "4.3.0"),
+            of("4.0.0", arrayOf("--increment=major", "--amount=3"), "7.0.0"),
+            of("4.0.0", arrayOf("--increment=PATCH", "--amount=3"), "4.0.3"),
+            of("4.0.0", arrayOf("--increment=MINOR", "--amount=3"), "4.3.0"),
+            of("4.0.0", arrayOf("--increment=MAJOR", "--amount=3"), "7.0.0"),
+
+            of("1.2.3", arrayOf("--amount=0", "--pre-release=abc"), "1.2.3-abc"),
+            of("1.2.3", arrayOf("--pre-release=abc"), "1.2.4-abc"),
+            of("1.2.3-abc", arrayOf("--pre-release=abc"), "1.2.4-abc"),
+            of("1.2.3+def", arrayOf("--pre-release=abc"), "1.2.4-abc"),
+            of("1.2.3-abc", arrayOf("--pre-release=def.123"), "1.2.4-def.123"),
+            of("1.2.3-abc+def", arrayOf("--pre-release=123.456", "--pre-release=789"), "1.2.4-123.456.789"),
+
+            of("1.2.3", arrayOf("--preserve", "--amount=0", "--pre-release=abc"), "1.2.3-abc"),
+            of("1.2.3", arrayOf("--preserve", "--pre-release=abc"), "1.2.4-abc"),
+            of("1.2.3-abc", arrayOf("--preserve", "--pre-release=abc"), "1.2.4-abc.abc"),
+            of("1.2.3+def", arrayOf("--preserve", "--pre-release=abc"), "1.2.4-abc+def"),
+            of("1.2.3-abc", arrayOf("--preserve", "--pre-release=def.123"), "1.2.4-abc.def.123"),
+            of(
+                "1.2.3-abc+def",
+                arrayOf("--preserve", "--pre-release=123.456", "--pre-release=789"),
+                "1.2.4-abc.123.456.789+def"
+            ),
+
+            of("1.2.3", arrayOf("--amount=0", "--build=abc"), "1.2.3+abc"),
+            of("1.2.3", arrayOf("--build=abc"), "1.2.4+abc"),
+            of("1.2.3-abc", arrayOf("--build=abc"), "1.2.4+abc"),
+            of("1.2.3+def", arrayOf("--build=abc"), "1.2.4+abc"),
+            of("1.2.3-abc", arrayOf("--build=def.123"), "1.2.4+def.123"),
+            of("1.2.3-abc+def", arrayOf("--build=123.456", "--build=789"), "1.2.4+123.456.789"),
+
+            of("1.2.3", arrayOf("--preserve", "--amount=0", "--build=abc"), "1.2.3+abc"),
+            of("1.2.3", arrayOf("--preserve", "--build=abc"), "1.2.4+abc"),
+            of("1.2.3-abc", arrayOf("--preserve", "--build=abc"), "1.2.4-abc+abc"),
+            of("1.2.3+def", arrayOf("--preserve", "--build=abc"), "1.2.4+def.abc"),
+            of("1.2.3-abc", arrayOf("--preserve", "--build=def.123"), "1.2.4-abc+def.123"),
+            of("1.2.3-abc+def", arrayOf("--preserve", "--build=123.456", "--build=789"), "1.2.4-abc+def.123.456.789")
+        )
+
+    }
 
 }
