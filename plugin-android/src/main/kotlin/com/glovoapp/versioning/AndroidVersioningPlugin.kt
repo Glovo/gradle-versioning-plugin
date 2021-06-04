63f487f8b3db0395e111af4e3e115ea15dc5e8c1
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-06-04 20:11:50 +0200

    [MOB-428] Fixed plugins apply order

--- plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
+++ plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
@@ -1,7 +1,6 @@
 package com.glovoapp.versioning
 
 import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.api.AndroidBasePlugin
 import com.glovoapp.versioning.SemanticVersioningPlugin.Companion.GROUP
 import com.glovoapp.versioning.tasks.IncrementNumericVersionTask
 import com.glovoapp.versioning.tasks.IncrementSemanticVersionTask
@@ -9,7 +8,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.register
-import org.gradle.kotlin.dsl.withType
 
 class AndroidVersioningPlugin : Plugin<Project> {
 
@@ -19,46 +17,48 @@ class AndroidVersioningPlugin : Plugin<Project> {
     }
 
     override fun apply(target: Project): Unit = with(target) {
+        check(plugins.findPlugin("com.android.application") != null) {
+            "${this::class.java} requires `com.android.application` to be applied first"
+        }
+
         val persistedProperties = rootProject.plugins.apply(PersistedVersionPlugin::class.java).persistedProperties
 
-        plugins.withType<AndroidBasePlugin> {
-            val numericVersion = if (KEY_VERSION_CODE in persistedProperties.keys)
-                persistedProperties.numericVersion(key = KEY_VERSION_CODE) else null
-            val semanticVersion = if (KEY_VERSION_NAME in persistedProperties.keys)
-                persistedProperties.semanticVersion(key = KEY_VERSION_NAME) else null
+        val numericVersion = if (KEY_VERSION_CODE in persistedProperties.keys)
+            persistedProperties.numericVersion(key = KEY_VERSION_CODE) else null
+        val semanticVersion = if (KEY_VERSION_NAME in persistedProperties.keys)
+            persistedProperties.semanticVersion(key = KEY_VERSION_NAME) else null
 
-            configure<BaseExtension> {
-                defaultConfig {
-                    numericVersion?.onChanged { versionCode = it }
-                    semanticVersion?.onChanged { versionName = it.toString() }
-                }
+        configure<BaseExtension> {
+            defaultConfig {
+                numericVersion?.onChanged { versionCode = it }
+                semanticVersion?.onChanged { versionName = it.toString() }
             }
+        }
 
-            val incrementVersionCodeTask = numericVersion?.let {
-                tasks.register<IncrementNumericVersionTask>("incrementVersionCode") {
-                    group = GROUP
-                    description = "Increments the project's versionCode by 1"
-                    version = numericVersion
-                }
+        val incrementVersionCodeTask = numericVersion?.let {
+            tasks.register<IncrementNumericVersionTask>("incrementVersionCode") {
+                group = GROUP
+                description = "Increments the project's versionCode by 1"
+                version = numericVersion
             }
+        }
 
-            val incrementVersionNameTask = semanticVersion?.let {
-                tasks.register<IncrementSemanticVersionTask>("incrementVersionName") {
-                    group = GROUP
-                    description = "Increments the project's versionName by 1"
-                    version = semanticVersion
-                }
+        val incrementVersionNameTask = semanticVersion?.let {
+            tasks.register<IncrementSemanticVersionTask>("incrementVersionName") {
+                group = GROUP
+                description = "Increments the project's versionName by 1"
+                version = semanticVersion
             }
+        }
 
-            // ensures these task are run (if present in the graph) before any Android build task
-            tasks.named("preBuild") {
-                shouldRunAfter(
-                    *listOfNotNull(
-                        incrementVersionCodeTask,
-                        incrementVersionNameTask
-                    ).toTypedArray()
-                )
-            }
+        // ensures these task are run (if present in the graph) before any Android build task
+        tasks.named("preBuild") {
+            shouldRunAfter(
+                *listOfNotNull(
+                    incrementVersionCodeTask,
+                    incrementVersionNameTask
+                ).toTypedArray()
+            )
         }
     }
 
