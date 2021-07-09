12a8d00d7c4236099dc4c830cf00c644fe0b5d66
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-07-09 14:12:06 +0200

    [MOB-477] Migrated to Property API

--- plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementNumericVersionTask.kt
+++ plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementNumericVersionTask.kt
@@ -6,19 +6,20 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
+import org.gradle.kotlin.dsl.property
 
 open class IncrementNumericVersionTask : DefaultTask() {
 
     @Internal
-    lateinit var version: PersistedVersion<Int>
+    val version = project.objects.property<PersistedVersion<Int>>()
 
     @Input
     @Option(option = "amount", description = "The amount to increment the numeric version")
-    var amount = 1
+    val amount = project.objects.property<Int>().convention(1)
 
     @TaskAction
     fun run() {
-        version.value += amount
+        version.get().value += amount.get()
     }
 
 }
--- plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementSemanticVersionTask.kt
+++ plugin/src/main/kotlin/com/glovoapp/versioning/tasks/IncrementSemanticVersionTask.kt
@@ -7,67 +7,78 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
+import org.gradle.kotlin.dsl.listProperty
+import org.gradle.kotlin.dsl.property
 
 open class IncrementSemanticVersionTask : DefaultTask() {
 
     @Internal
-    lateinit var version: PersistedVersion<SemanticVersion>
+    val version = project.objects.property<PersistedVersion<SemanticVersion>>()
 
     @Input
     @Option(option = "increment", description = "The increment type")
-    var increment = SemanticVersion.Increment.PATCH
+    val increment = project.objects.property<SemanticVersion.Increment>().convention(SemanticVersion.Increment.PATCH)
 
     @Input
-    var amount = 1
+    val amount = project.objects.property<Int>().convention(1)
 
     @Input
     @Option(option = "preserve-pre-release", description = "Add or replace current pre-release labels")
-    var preservePreRelease = false
+    val preservePreRelease = project.objects.property<Boolean>().convention(false)
 
     @Input
     @Option(option = "preserve-build", description = "Add or replace current build labels")
-    var preserveBuild = false
+    val preserveBuild = project.objects.property<Boolean>().convention(false)
 
     @Input
-    @Option(option = "pre-release", description = "Adds pre-release labels to the version")
-    var preReleaseLabels = mutableListOf<String>()
+    val preReleaseLabels = project.objects.listProperty<String>()
 
     @Input
-    @Option(option = "build", description = "Adds build labels to the version")
-    var buildLabels = mutableListOf<String>()
+    val buildLabels = project.objects.listProperty<String>()
 
     @Option(option = "major", description = "Increments major version")
     fun major() {
-        this.increment = SemanticVersion.Increment.MAJOR
+        this.increment.set(SemanticVersion.Increment.MAJOR)
     }
 
     @Option(option = "minor", description = "Increments minor version")
     fun minor() {
-        this.increment = SemanticVersion.Increment.MINOR
+        this.increment.set(SemanticVersion.Increment.MINOR)
     }
 
     @Option(option = "patch", description = "Increments patch version")
     fun patch() {
-        this.increment = SemanticVersion.Increment.PATCH
+        this.increment.set(SemanticVersion.Increment.PATCH)
     }
 
     @Option(option = "amount", description = "The increment amount")
     fun amount(amount: String) {
-        this.amount = checkNotNull(amount.toIntOrNull()?.takeIf { it >= 0 }) { "amount `$amount` must be >= 0" }
+        this.amount.set(checkNotNull(amount.toIntOrNull()?.takeIf { it >= 0 }) { "amount `$amount` must be >= 0" })
     }
 
     @Option(option = "preserve", description = "Add or replace current pre-release and build labels")
     fun preserve() {
-        this.preservePreRelease = true
-        this.preserveBuild = true
+        this.preservePreRelease.set(true)
+        this.preserveBuild.set(true)
+    }
+
+    @Option(option = "pre-release", description = "Adds pre-release labels to the version")
+    fun preReleaseLabels(labels: List<String>) {
+        preReleaseLabels.set(labels)
+    }
+
+    @Option(option = "build", description = "Adds build labels to the version")
+    fun buildLabels(labels: List<String>) {
+        buildLabels.set(labels)
     }
 
     @TaskAction
-    fun run() = with(version) {
-        val prLabels = if (preservePreRelease) value.preReleaseIdentifiers + preReleaseLabels else preReleaseLabels
-        val bLabels = if (preservePreRelease) value.buildIdentifiers + buildLabels else buildLabels
+    fun run() = with(version.get()) {
+        fun List<String>.ifPreserved() = if (preservePreRelease.get()) this else emptyList()
+        val prLabels = value.preReleaseIdentifiers.ifPreserved() + preReleaseLabels.get()
+        val bLabels = value.buildIdentifiers.ifPreserved() + buildLabels.get()
 
-        value = (value + increment * amount).copy(
+        value = (value + increment.get() * amount.get()).copy(
             preRelease = prLabels.joinToString(separator = "."),
             build = bLabels.joinToString(separator = ".")
         )
