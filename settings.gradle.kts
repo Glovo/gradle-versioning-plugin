f385fc0511300ffeb96430233c98a9e2086d0522
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-07-14 23:42:59 +0200

    [MOB-490] Local publications report

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.glovoapp.artifactory") version "0.1.12"
-    id("com.glovoapp.semantic-versioning") version "0.1.7"
-}
-
 include(":library")
 include(":plugin")
 include(":plugin-android")
