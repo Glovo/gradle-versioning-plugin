258b7436282b5c12935fc881bbd0ee4cfa2b8733
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-08-09 14:10:45 +0200

    [MOB-490] Added `com.gradle.plugin-publish` plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `kotlin-dsl`
     `java-test-fixtures`
     id("pl.droidsonroids.jacoco.testkit")
+    id("com.gradle.plugin-publish") version "0.15.0"
 }
 
 base.archivesName.set("gradle-versioning-plugin")
@@ -14,6 +15,14 @@ gradlePlugin {
             implementationClass = "com.glovoapp.versioning.SemanticVersioningPlugin"
         }
     }
+}
+
+pluginBundle {
+    val repoUrl = "https://github.com/Glovo/gradle-versioning-plugin"
+    website = repoUrl
+    vcsUrl = repoUrl
+    description = "A Gradle plugin implementing https://semver.org/"
+    tags = listOf("gradle", "semantic-versioning", "java", "kotlin", "android")
 }
 
 dependencies {
