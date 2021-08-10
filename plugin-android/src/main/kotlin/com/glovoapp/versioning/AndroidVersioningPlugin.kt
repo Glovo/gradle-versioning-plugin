99e3f906f44ab032765a2637338fecfa513c5b0e
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-08-10 14:53:00 +0200

    Updated AGP version to `7.0.0`

--- plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
+++ plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
@@ -2,7 +2,6 @@ package com.glovoapp.versioning
 
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.api.AndroidBasePlugin
-import com.android.build.gradle.internal.plugins.BasePlugin
 import com.glovoapp.versioning.SemanticVersioningPlugin.Companion.GROUP
 import com.glovoapp.versioning.SemanticVersioningPlugin.Companion.ensureGradleVersion
 import com.glovoapp.versioning.tasks.IncrementNumericVersionTask
@@ -25,7 +24,7 @@ class AndroidVersioningPlugin : Plugin<Project> {
                 .extensions.create<AndroidVersioningExtension>("versioning")
         }
 
-        plugins.withType<BasePlugin<*, *>> {
+        plugins.withType<AndroidBasePlugin> {
             @Suppress("UNUSED_EXPRESSION")
             extension // early init so the extension will be available for scripting under `android.versioning`
         }
