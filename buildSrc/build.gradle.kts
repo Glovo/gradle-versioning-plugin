f038e4f6d793b73ace30f855e5ccf196bcbe7265
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2021-08-20 15:00:23 +0200

    [MOB-490] Updated release flow

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 
 dependencies {
     implementation(embeddedKotlin("gradle-plugin"))
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.3")
     implementation("gradle.plugin.pl.droidsonroids.gradle.jacoco:jacoco-gradle-testkit-plugin:1.0.9")
     implementation("com.gradle.publish:plugin-publish-plugin:0.15.0")
