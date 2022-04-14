ff38f24b76e8c778b434933c9480f31b772d6b08
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2022-04-14 14:01:43 +0200

    [MOB-916] Fixed AGP `7.1.2` too late to change issue

--- plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
+++ plugin-android/src/main/kotlin/com/glovoapp/versioning/AndroidVersioningPlugin.kt
@@ -1,5 +1,6 @@
 package com.glovoapp.versioning
 
+import com.android.build.api.variant.AndroidComponentsExtension
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.api.AndroidBasePlugin
 import com.glovoapp.versioning.SemanticVersioningPlugin.Companion.GROUP
@@ -11,8 +12,10 @@ import org.gradle.api.Project
 import org.gradle.api.internal.plugins.DslObject
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.getByName
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
+import kotlin.reflect.KMutableProperty
 
 class AndroidVersioningPlugin : Plugin<Project> {
 
@@ -24,58 +27,83 @@ class AndroidVersioningPlugin : Plugin<Project> {
                 .extensions.create<AndroidVersioningExtension>("versioning")
         }
 
-        plugins.withType<AndroidBasePlugin> {
-            @Suppress("UNUSED_EXPRESSION")
-            extension // early init so the extension will be available for scripting under `android.versioning`
-        }
-
         afterEvaluate {
             check(plugins.hasPlugin(AndroidBasePlugin::class.java)) {
                 "${this@AndroidVersioningPlugin::class.java} requires an `android` to be applied"
             }
+        }
 
-            val androidVersion = extension.version.get()
+        plugins.withType<AndroidBasePlugin> {
+            @Suppress("UNUSED_EXPRESSION")
+            extension // early init so the extension will be available for scripting under `android.versioning`
 
-            val incrementVersionCodeTask = androidVersion.code?.let {
-                tasks.register<IncrementNumericVersionTask>("incrementVersionCode") {
-                    group = GROUP
-                    description = "Increments the project's versionCode by given `amount`"
-                    version.value(it).disallowChanges()
-                }
-            }
+            extensions
+                .getByName<AndroidComponentsExtension<*, *, *>>("androidComponents")
+                .finalizeDsl {
+                    val androidVersion = extension.version.get()
 
-            val incrementVersionNameTask = androidVersion.name?.let {
-                tasks.register<IncrementSemanticVersionTask>("incrementVersionName") {
-                    group = GROUP
-                    description = "Increments the project's versionName by given `increment`"
-                    version.value(it).disallowChanges()
-                }
-            }
+                    val incrementVersionCodeTask = androidVersion.code?.let {
+                        tasks.register<IncrementNumericVersionTask>("incrementVersionCode") {
+                            group = GROUP
+                            description = "Increments the project's versionCode by given `amount`"
+                            version.value(it).disallowChanges()
+                        }
+                    }
 
-            configure<BaseExtension> {
-                defaultConfig {
-                    androidVersion.code?.onChanged { versionCode = it }
-                    androidVersion.name?.onChanged { versionName = it.toString(); }
-                }
-            }
+                    val incrementVersionNameTask = androidVersion.name?.let {
+                        tasks.register<IncrementSemanticVersionTask>("incrementVersionName") {
+                            group = GROUP
+                            description =
+                                "Increments the project's versionName by given `increment`"
+                            version.value(it).disallowChanges()
+                        }
+                    }
 
-            // ensures these task are run (if present in the graph) before any Android build task
-            tasks.named("preBuild") {
-                doFirst {
-                    checkNotNull(androidVersion.code ?: androidVersion.name) {
-                        "Please provide at least one of '${extension.versionCodeProperty.get()}' or " +
-                                "'${extension.versionCodeProperty.get()}' properties on " +
-                                "`${extension.propertiesFile.get()}`"
+                    configure<BaseExtension> {
+                        defaultConfig {
+                            fun <Type : Any, PropType> PersistedVersion<Type>.bind(
+                                property: KMutableProperty<PropType?>,
+                                transform: (Type) -> PropType
+                            ) {
+                                val propValue = transform(value)
+
+                                property.setter.call(propValue)
+
+                                onChanged {
+                                    val current = property.getter.call()
+
+                                    if (propValue != current) {
+                                        error("""
+                                        $name has changed but not reflected into Android build:
+                                        before: $current
+                                        after: $propValue
+                                        """.trimIndent())
+                                    }
+                                }
+                            }
+
+                            androidVersion.code?.bind(::versionCode) { it }
+                            androidVersion.name?.bind(::versionName) { it.toString() }
+                        }
+                    }
+
+                    // ensures these task are run (if present in the graph) before any Android build task
+                    tasks.named("preBuild") {
+                        doFirst {
+                            checkNotNull(androidVersion.code ?: androidVersion.name) {
+                                "Please provide at least one of '${extension.versionCodeProperty.get()}' or " +
+                                        "'${extension.versionCodeProperty.get()}' properties on " +
+                                        "`${extension.propertiesFile.get()}`"
+                            }
+                        }
+                        shouldRunAfter(
+                            *listOfNotNull(
+                                incrementVersionCodeTask,
+                                incrementVersionNameTask
+                            ).toTypedArray()
+                        )
                     }
                 }
-                shouldRunAfter(
-                    *listOfNotNull(
-                        incrementVersionCodeTask,
-                        incrementVersionNameTask
-                    ).toTypedArray()
-                )
-            }
         }
     }
-
 }
